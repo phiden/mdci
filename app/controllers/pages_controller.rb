b6581c3c6b259c267651ecae7a07b144d077c02c
phiden <sdengo@codeforamerica.org>
2015-08-25 16:58:01 -0700

    added static Pages controller and a login landing page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,2 @@
-class PagesController < ActionController::Base
-  def login
-    # put any code here that you need 
-    # (although for a static view you probably won't have any)
-  end
+class PagesController < ApplicationController
 end
