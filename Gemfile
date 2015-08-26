e1a2633f8c7cae83a7844536282b815b47ba03b6
phiden <sdengo@codeforamerica.org>
2015-08-26 10:54:25 -0700

    removed bootstrap CSS

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'rails', '4.1.5'
 gem 'pg'
 
 # Use SCSS for stylesheets
-gem 'bootstrap-sass', '~> 3.2.0'
+# gem 'bootstrap-sass', '~> 3.2.0'
 gem 'sass-rails', '~> 4.0.3'
 
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825180106) do
+ActiveRecord::Schema.define(version: 20150826001421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hashtags", force: true do |t|
+    t.string   "tag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lat_lngs", force: true do |t|
+    t.string   "lat"
+    t.string   "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -29,6 +42,13 @@ ActiveRecord::Schema.define(version: 20150825180106) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
