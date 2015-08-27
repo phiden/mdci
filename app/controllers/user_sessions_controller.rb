065be44055fed1bec7aa454d12c5a1a3b371e0fe
phiden <sdengo@codeforamerica.org>
2015-08-27 16:52:31 -0700

    authentication now protects the whole app

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,7 @@
 class UserSessionsController < ApplicationController
+	
+	skip_before_filter :require_login, except: [:destroy]
+	
   def new
     @user = User.new
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	
+	skip_before_filter :require_login, only: [:index, :new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -28,7 +30,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        redirect_to(:users, notice: 'User was successfully created')
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
