b47f00b90c7c024eefc0f3d249bef59726ec0b0c
phiden <sdengo@codeforamerica.org>
2015-08-27 16:48:01 -0700

    login/logout working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   
-  resources :users
+  
+
+	# root :to => 'users#index'
+	resources :user_sessions
+	resources :users
+	
+	get 'login' => 'user_sessions#new', :as => :login
+	post 'logout' => 'user_sessions#destroy', :as => :logout
 
   root 'pages#map'
 
